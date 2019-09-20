e91b3d0c0061cb0d862386ce029dfb16a18d43d3
Yuki Uchiyama <yuki.uchiyama0414@gmail.com>
2019-09-20 08:00:29 +0000

    add sign up menu

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,7 @@ class UserTest < ActiveSupport::TestCase
   def setup
    @user = User.new(name: "Example User", email: "user@example.com",
            password: "foobar", password_confirmation: "foobar")
- end
+  end
 
   test "should be valid" do
     assert @user.valid?
